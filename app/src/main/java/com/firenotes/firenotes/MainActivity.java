f0d64eefe404b4d547a25d25b27fe379cfbddf6e
akshay9732 <akshay9732@gmail.com>
2017-06-24 12:21:09 +0530

    Initial commit
    Initial commit

---
+++ app/src/main/java/com/firenotes/firenotes/MainActivity.java
@@ -0,0 +1,16 @@
+package com.firenotes.firenotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  //  private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  mAuth = FirebaseAuth.getInstance();
+    }
+}
---
+++ app/src/main/java/com/firenotes/firenotes/models/Note.java
@@ -0,0 +1,9 @@
+package com.firenotes.firenotes.models;
+
+/**
+ * Created by WIN 10 on 6/24/2017.
+ */
+
+public class Note {
+
+}
